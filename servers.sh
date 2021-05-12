696187cd8f8d08d288f5ac1a93d8e2ec844e1524
iAmG-r00t <iAmGr00t@random.com>
2021-05-13 01:59:27 +0300

    Some few edits for the server installation script 🔥 🔥 🔥 🔥 🔥

--- servers.sh
+++ servers.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 # Script to setup afew things in a server
 
+# author: iAmG-r00t a.k.a th3_gr00t a.k.a Peter Numi.
+
+# Debug mode
+#set -x
+
+# Exit or error
+set -e
+
 # colors
 bold=`tput bold`
 reset='\033[0m'
 blue='\e[1;34m'
 green='\e[1;32m'
 brown='\e[0;33m'
+magenta="\e[95m"
+red="\033[1;31m"
 no_color='\e[0m'
 blink='\e[5m'
 
+
+
+# Report errors when file exits out
+filename=$(basename "$BASH_SOURCE")
+err_report() {
+  echo " "
+  echo -e "* ${brown}Error on line ${red}$1${reset} file: ${magenta}${filename}${reset}"
+  echo " "
+}
+
+trap 'err_report $LINENO' ERR
 
 echo -e "\n"
 echo -e "${bold}[+] ${reset}${brown}First doing a simple update and upgrade.\n${reset}"
